3d3979f54b8e2151a02b8cad74dc7f94052ad96a
samuele301197 <parisisamu32@gmail.com>
2025-07-11 16:52:37 +0200

    feat:cambio nome percorso immagine

--- middleware/imagePath.js
+++ middleware/imagePath.js
@@ -1,5 +1,5 @@
 const imagePath = (req, res, next) => {
-    const percorsoImmagine = `${req.protocol}://${req.get("host")}/images/shoes`;
+    const percorsoImmagine = `${req.protocol}://${req.get("host")}/img/shoes`;
     req.imagePath = percorsoImmagine;
     next();
 
