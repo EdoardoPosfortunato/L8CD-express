8f63bfa14c66882d3295f5c5bccb3e3348f9ae02
qsrdev <matteo.albertini747@gmail.com>
2025-07-11 11:46:50 +0200

    feat: folder fix

--- middleware/imagePath.js
+++ middleware/imagePath.js
@@ -1,9 +1,7 @@
-const imagePath = (req, res ,next) => {
-
-    const percorsoImmagine = `${req.protocol}://${req.get("host")}/images`;
-    req.imagePath = percorsoImmagine;
-    next();
-
-}
+const imagePath = (req, res, next) => {
+  const percorsoImmagine = `${req.protocol}://${req.get("host")}/images`;
+  req.imagePath = percorsoImmagine;
+  next();
+};
 
 export default imagePath;
