2404b28a2294d02e361657f48deb983d1772a04d
samuele301197 <parisisamu32@gmail.com>
2025-07-14 11:43:05 +0200

    feat: filtri

--- controllers/shoesController.js
+++ controllers/shoesController.js
@@ -1,74 +1,96 @@
 import connection from "../db.js";
 import slugify from "slugify";
-import imagePath from "../middleware/imagePath.js";
 
-//index
-const index = (req, res) => {
-  const gender = req.query.gender;
-  const isNew = req.query.isNew;
 
-  let sql = "SELECT * from products";
+const index = (req, res, next) => {
+  const { gender, isNew, minPrice, maxPrice, brand } = req.query;
+
+  let sql = "SELECT * FROM products";
+  let conditions = [];
   let params = [];
-  
+
 
   if (isNew === "true") {
-    sql += " WHERE price >= ?";
+    conditions.push("price >= ?");
     params.push(160);
-  } else if (gender) {
-    sql += " WHERE gender = ?";
+  }
+
+ 
+  if (gender) {
+    conditions.push("gender = ?");
     params.push(gender);
   }
 
-  connection.query(sql, params, (err, result) => {
-    if (err) {
-      return next(new Error(err));
-    }
 
-    const shoes = result.map((curShoe) => {
-      return {
-        ...curShoe,
-        image: curShoe.image ? `${req.imagePath}/${curShoe.image}` : null
-      };
-    });
+  if (brand) {
+    conditions.push("brand LIKE ?");
+    params.push(`%${brand}%`);
+  }
+
+
+  if (minPrice) {
+    conditions.push("price >= ?");
+    params.push(minPrice);
+  }
+
+
+  if (maxPrice) {
+    conditions.push("price <= ?");
+    params.push(maxPrice);
+  }
+
+
+  if (conditions.length > 0) {
+    sql += " WHERE " + conditions.join(" AND ");
+  }
+
+
+  connection.query(sql, params, (err, result) => {
+    if (err) return next(new Error(err));
+
+    const shoes = result.map((curShoe) => ({
+      ...curShoe,
+      image: curShoe.image ? `${req.imagePath}/${curShoe.image}` : null,
+    }));
 
     res.status(200).json({
       info:
         isNew === "true"
-        ? "Scarpe novità (prezzo ≥ 160)"
-        : gender
-        ? `Scarpe per genere: ${gender}`
-        : "Tutte le scarpe",
+          ? "Scarpe novità (prezzo ≥ 160)"
+          : gender
+          ? `Scarpe per genere: ${gender}`
+          : "Tutte le scarpe",
       totalcount: result.length,
       data: shoes,
     });
   });
 };
 
-//show
+
 const show = (req, res) => {
   const id = req.params.id;
 
-  const shoeRequest = `SELECT products.*
-                         FROM products
-                         WHERE products.id = ?;`;
+  const shoeRequest = `SELECT * FROM products WHERE id = ?`;
 
   connection.query(shoeRequest, [id], (err, result) => {
-    if (err) {
+    if (err || result.length === 0) {
       return res.status(404).json({
         status: "404",
         info: "Scarpa non trovata",
       });
-    } else {
-      const shoeData = result[0];
-      res.status(200).json({
-        data: {
-          ...shoeData,
-          image: shoeData.image ? `${req.imagePath}/${shoeData.image}` : null
-        },
-      });
     }
+
+    const shoeData = result[0];
+
+    res.status(200).json({
+      data: {
+        ...shoeData,
+        image: shoeData.image ? `${req.imagePath}/${shoeData.image}` : null,
+      },
+    });
   });
 };
+
 
 const shoesController = {
   index,
