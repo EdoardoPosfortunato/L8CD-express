9b0868f607eecae33116e9576a88a78a23b0d4c3
samuele301197 <parisisamu32@gmail.com>
2025-07-15 17:37:52 +0200

    feat: puma mb 02 img

--- database/2025-07-15-l8cd-modifica.sql
+++ database/2025-07-15-l8cd-modifica.sql
@@ -4,3 +4,5 @@ ALTER TABLE invoices
 MODIFY updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE invoices
 MODIFY created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+UPDATE `l8cd_db`.`products` SET `image` = 'Puma-MB-02.webp' WHERE (`id` = '39');
