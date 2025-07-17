4c8b80edb968bb647143fb8300b1e47d82256dd6
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-07-17 12:04:16 +0200

    feat: implementing chatbot AI Gemini

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import notFound from "./middleware/notFound.js";
 import errorHandler from "./middleware/errorHandler.js";
 import imagePath from "./middleware/imagePath.js";
+import { GoogleGenAI } from "@google/genai";
 
 const app = express();
 const port = process.env.SERVER_PORT;
@@ -13,6 +14,8 @@ const port = process.env.SERVER_PORT;
 app.use(express.json());
 //impostazione della cartella public per le immagini
 app.use(express.static("public"));
+
+const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
 // connessione con react /////
 app.use(
@@ -27,6 +30,37 @@ app.get("/", (req, res) => {
     data: "benvenuto nel API delle scarpe",
   });
 });
+
+app.post('/api/chat', async (req, res) => {
+  const { message } = req.body
+
+
+  try {
+
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.setHeader("Transfer-Encoding", "chunked");
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+
+    const response = await ai.models.generateContentStream({
+      model: 'gemini-2.0-flash-001',
+      contents: message,
+    });
+
+    for await (const chunk of response) {
+      const text = chunk.text || "";
+      console.log("Chunk:", text);
+      res.write(text);
+    }
+
+    res.end();
+
+    } catch (error) {
+      console.error('Error', error.message);
+      res.status(500).json({error: 'Something went wrong'})
+    }
+})
 
 app.use("/shoes", imagePath, router);
 
