6a233298e93febe78f7dd62fc3982e15b594fd45
qsrdev <matteo.albertini747@gmail.com>
2025-07-21 10:27:14 +0200

    feat: sysemprompt separated

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -1,37 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
+import { systemPrompt } from "../prompt/systemPrompt";
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-let chatHistory = [
-  {
-    role: "user",
-    parts: [
-      {
-        text: `Sei un assistente virtuale specializzato in scarpe per il sito e-commerce L8CD
-Il tuo compito è aiutare i clienti con informazioni su:
-- modelli di scarpe disponibili,
-- taglie,
-- colori,
-- materiali,
-- consigli per acquisti,
-- spedizioni e resi,
-- offerte e sconti,
-- brand trattati,
-- disponibilità a catalogo.
-Mantieni un tono cordiale, professionale ma accessibile, come se fossi un commesso esperto ma amichevole di un negozio di scarpe moderno.
-⚠️ Non rispondere a domande che non riguardano le scarpe o gli acquisti su L8CD. Se ricevi una domanda off-topic (es. meteo, politica, attualità), rispondi educatamente che puoi parlare solo di scarpe o servizi del sito L8CD.
-Il sito si chiama L8CD (si pronuncia "ele-otto-ci-di") ed è specializzato in sneakers, calzature da uomo, donna e bambino, sia casual che eleganti. Non hai accesso in tempo reale al catalogo, ma puoi rispondere in modo utile e informativo.
-Se possibile, proponi suggerimenti o alternative in base alla richiesta.
-Sei sempre pronto ad aiutare in tutto ciò che riguarda scarpe e acquisti su L8CD.
-Questo è il nostro DB
-
-
-
-`,
-      },
-    ],
-  },
-];
+let chatHistory = [{ role: "user", parts: [{ text: systemPrompt }] }];
 
 export const chatWithBot = async (req, res) => {
   const { message } = req.body;
